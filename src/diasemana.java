7fa6044259404237fbc727fbd35484494fd1340d
juancargnz <JuanCarlosGonzalezLe@CSESTPC16044-CA>
2023-11-09 12:10:07 +0100

    dias y meses

--- src/diasemana.java
+++ src/diasemana.java
@@ -7,29 +7,29 @@ public class diasemana {
 		int mes=1;
 		while (mes<13) { 
 			if (mes==1) {
-				System.out.print("Enero");
+				System.out.print("Enero"+(" "));
 			}else if (mes==2){
-				System.out.print("Febrero");
+				System.out.print("Febrero"+(" "));
 			}else if(mes==3){
-				System.out.print("Marzo");
+				System.out.print("Marzo"+(" "));
 			}else if(mes==4) {
-				System.out.print("Abril");
+				System.out.print("Abril"+(" "));
 			}else if(mes==5) {
-				System.out.print("Mayo");
+				System.out.print("Mayo"+(" "));
 			}else if (mes==6){
-				System.out.print("Junio");
+				System.out.print("Junio"+(" "));
 			}else if(mes==7){
-				System.out.print("Julio");
+				System.out.print("Julio"+(" "));
 			}else if(mes==8) {
-				System.out.print("Agosto");
+				System.out.print("Agosto"+(" "));
 			}else if(mes==9) {
-				System.out.print("Septiembre");
+				System.out.print("Septiembre"+(" "));
 			}else if (mes==10){
-				System.out.print("Octubre");
+				System.out.print("Octubre"+(" "));
 			}else if(mes==11){
-				System.out.print("Noviembre");
+				System.out.print("Noviembre"+(" "));
 			}else if(mes==12) {
-				System.out.print("Diciembre");
+				System.out.print("Diciembre"+(" "));
 			}
 			
 			while (semana < 4) {
