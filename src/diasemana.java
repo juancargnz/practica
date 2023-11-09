d14d03918dd6ae123eebfe96ee47809b138740c9
juancargnz <JuanCarlosGonzalezLe@CSESTPC16044-CA>
2023-11-09 12:06:24 +0100

    dias meses del año

--- src/diasemana.java
+++ src/diasemana.java
@@ -5,7 +5,33 @@ public class diasemana {
 		int diasemana = 1;
 		int semana=0;
 		int mes=1;
-		while (mes<13) {
+		while (mes<13) { 
+			if (mes==1) {
+				System.out.print("Enero");
+			}else if (mes==2){
+				System.out.print("Febrero");
+			}else if(mes==3){
+				System.out.print("Marzo");
+			}else if(mes==4) {
+				System.out.print("Abril");
+			}else if(mes==5) {
+				System.out.print("Mayo");
+			}else if (mes==6){
+				System.out.print("Junio");
+			}else if(mes==7){
+				System.out.print("Julio");
+			}else if(mes==8) {
+				System.out.print("Agosto");
+			}else if(mes==9) {
+				System.out.print("Septiembre");
+			}else if (mes==10){
+				System.out.print("Octubre");
+			}else if(mes==11){
+				System.out.print("Noviembre");
+			}else if(mes==12) {
+				System.out.print("Diciembre");
+			}
+			
 			while (semana < 4) {
 				while (diasemana <= 7) {
 					if (diasemana == 1) {
