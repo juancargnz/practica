685552c6ba2c6026c689d51085ee08db9a7dbcb1
juancargnz <JuanCarlosGonzalezLe@CSESTPC16044-CA>
2023-11-09 11:51:30 +0100

    meses y años

--- src/diasemana.java
+++ src/diasemana.java
@@ -1,30 +1,40 @@
 
 public class diasemana {
 	public static void main(String[] args) {
-		System.out.println();
-		int diasemana = 1;
-
-		while (diasemana <= 7) {
-			if (diasemana==1) {
-				System.out.print("[" + "L"+" ");
-			} else if (diasemana==2) {
-				System.out.print("M"+" ");
-			}
-			if (diasemana==3) {
-				System.out.print("M"+" ");
-			} else if(diasemana==4) {
-				System.out.print("J"+" ");
-			}
-			if(diasemana==5) {
-				System.out.print("V"+" ");
-			} else if(diasemana==6) {
-				System.out.print("S"+" ");
-			}
-			if (diasemana==7) {
-				System.out.print("D"+"]");
-			}	
-			diasemana = diasemana + 1;
 		
+		int diasemana = 1;
+		int semana=0;
+		int mes=1;
+		while (mes<13) {
+			while (semana < 4) {
+				while (diasemana <= 7) {
+					if (diasemana == 1) {
+						System.out.print("[" + "L" + " ");
+					} else if (diasemana == 2) {
+						System.out.print("M" + " ");
+					}
+					if (diasemana == 3) {
+						System.out.print("M" + " ");
+					} else if (diasemana == 4) {
+						System.out.print("J" + " ");
+					}
+					if (diasemana == 5) {
+						System.out.print("V" + " ");
+					} else if (diasemana == 6) {
+						System.out.print("S" + " ");
+					}
+					if (diasemana == 7) {
+						System.out.print("D" + "]");
+					}
+					diasemana = diasemana + 1;
+
+				}
+				diasemana = 1;
+				semana = semana + 1;
+			} 
+			System.out.println();
+			semana=0;
+			mes =mes+1;
 		}		
 	}
 
